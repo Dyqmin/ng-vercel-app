712a207f70dbf3b6faaccc08483de8abfe7db090
Dominik Donoch <teesdomin@gmail.com>
2023-03-05 00:05:28 +0100

    feat: add `@vercel/analytics`

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { isDevMode } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
+import { inject } from "@vercel/analytics";
 
 import { AppComponent } from "./app/app.component";
 import { routes } from "./app/routes";
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
-}).catch(err => console.error(err));
+})
+  .then(() => (inject({ mode: isDevMode() ? 'development' : 'production'})))
+  .catch(err => console.error(err));
