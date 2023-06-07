dc49a4971ee7be4c40522340c67fb07de6439d2b
Dominik Donoch <teesdomin@gmail.com>
2023-06-06 22:43:20 -0400

    chore: add `@vercel/postgres`

--- api/handler.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function (request: VercelRequest, response: VercelResponse) {
-  const { name = 'World' } = request.query;
-  response.status(200).send({
-    message: `Hello ${name}!`,
-  });
-};
